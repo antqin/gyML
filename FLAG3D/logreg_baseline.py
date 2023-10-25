b34865f4be02fc0dfe7736afc51c5a847bade596
Anthony Qin <antqin27@stanford.edu>
2023-10-25 16:41:45 -0700

    Upload logreg and random forest files

---
+++ FLAG3D/logreg_baseline.py
@@ -0,0 +1,100 @@
+import os
+import numpy as np
+from joblib import dump, load
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+
+# Load data from .pkl files
+def load_data_from_directory(directory):
+    X = []
+    y = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".pkl"):
+            filepath = os.path.join(directory, filename)
+            try:
+                data = load(filepath)
+                poses_data = data['poses']
+                X.append(poses_data)  # Use only the 'poses' data as features
+                action = filename.split('A')[1][:3]  # Extracting the Axxx part
+                y.append(action)
+            except Exception as e:
+                print(f"Warning: Could not load {filename}. Error: {e}")
+                continue
+    return X, y
+
+# Function to pad arrays in a list to have the same length
+def pad_arrays(array_list):
+    # Determine the maximum length among all arrays
+    max_length = max(arr.shape[0] for arr in array_list)
+    
+    # Pad each array to match the maximum length
+    padded_list = []
+    for arr in array_list:
+        padded_arr = np.pad(arr, (0, max_length - arr.shape[0]))
+        padded_list.append(padded_arr)
+    
+    return np.array(padded_list)
+
+# Load train, dev, and test data
+X_train, y_train = load_data_from_directory('dataset/train')
+print(len(X_train))
+X_train = [x.flatten() for x in X_train]  # Flatten the 'poses' arrays for each training example
+
+X_dev, y_dev = load_data_from_directory('dataset/dev')
+X_dev = [x.flatten() for x in X_dev]  # Flatten the 'poses' arrays for each dev example
+
+X_test, y_test = load_data_from_directory('dataset/test')
+X_test = [x.flatten() for x in X_test]  # Flatten the 'poses' arrays for each test example
+
+X_train = pad_arrays(X_train)
+X_dev = pad_arrays(X_dev)
+X_test = pad_arrays(X_test)
+
+# Load the trained model
+# clf = load('logreg_model.pkl')
+
+# Initialize the Logistic Regression model with one-vs-rest strategy
+clf = LogisticRegression(max_iter=1000, multi_class='ovr', solver='saga', verbose=1)
+
+# Train the model
+clf.fit(X_train, y_train)
+dump(clf, 'logreg_model.pkl')
+
+# Predict on the dev set (or test set)
+y_pred_dev = clf.predict(X_dev)
+
+# Calculate the accuracy
+accuracy = accuracy_score(y_dev, y_pred_dev)
+print(f"Baseline Accuracy on Dev set: {accuracy:.2f}")
+
+# If the dev set performance is satisfactory, you can evaluate on the test set
+y_pred_test = clf.predict(X_test)
+accuracy_test = accuracy_score(y_test, y_pred_test)
+print(f"Accuracy on Test set: {accuracy_test:.2f}")
+
+# For Logistic Regression:
+clf_logistic = LogisticRegression(max_iter=10000, multi_class='ovr', solver='saga')
+clf_logistic.fit(X_train, y_train)
+y_pred_logistic = clf_logistic.predict(X_dev)
+
+# Confusion Matrix for Logistic Regression
+cm_logistic = confusion_matrix(y_dev, y_pred_logistic)
+plt.figure(figsize=(12, 10))
+sns.heatmap(cm_logistic, annot=True, fmt='g', cmap='Blues')
+plt.title('Confusion Matrix for Logistic Regression')
+plt.xlabel('Predicted labels')
+plt.ylabel('True labels')
+plt.savefig("confusion_matrix_logreg.png", dpi=300)
+plt.show()
+
+# Coefficients Visualization
+plt.figure(figsize=(15, 5))
+plt.bar(range(len(X_train[0])), clf_logistic.coef_[0])  # Change index for other classes
+plt.title('Feature Coefficients for Logistic Regression (Class 0)')
+plt.xlabel('Features')
+plt.ylabel('Coefficient Value')
+plt.savefig("feature_coefficients_logreg.png", dpi=300)
+plt.show()
---
+++ FLAG3D/random_forest.py
@@ -0,0 +1,93 @@
+import os
+import numpy as np
+from joblib import dump, load
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+
+# Load data from .pkl files
+def load_data_from_directory(directory):
+    X = []
+    y = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".pkl"):
+            filepath = os.path.join(directory, filename)
+            try:
+                data = load(filepath)
+                poses_data = data['poses']
+                X.append(poses_data)  # Use only the 'poses' data as features
+                action = filename.split('A')[1][:3]  # Extracting the Axxx part
+                y.append(action)
+            except Exception as e:
+                print(f"Warning: Could not load {filename}. Error: {e}")
+                continue
+    return X, y
+
+def pad_and_flatten(array_list):
+    # Determine the maximum length among all flattened arrays
+    max_length = max(arr.size for arr in array_list)
+    
+    # Pad each array to match the maximum length and then flatten
+    padded_list = []
+    for arr in array_list:
+        padded_arr = np.pad(arr, ((0, 0), (0, max_length - arr.size)))
+        flattened_padded_arr = padded_arr.flatten()
+        padded_list.append(flattened_padded_arr)
+    
+    return np.array(padded_list)
+
+# Load train, dev, and test data
+X_train, y_train = load_data_from_directory('dataset/train')
+X_dev, y_dev = load_data_from_directory('dataset/dev')
+X_test, y_test = load_data_from_directory('dataset/test')
+X_train = pad_and_flatten(X_train)
+X_dev = pad_and_flatten(X_dev)
+X_test = pad_and_flatten(X_test)
+
+# Load the trained model
+# clf = load('random_forest_model.pkl')
+
+# Initialize the Random Forest classifier
+clf = RandomForestClassifier(n_estimators=100, random_state=42, verbose=1)
+
+# Train the model
+clf.fit(X_train, y_train)
+dump(clf, 'random_forest_model.pkl')
+
+# Predict on the dev set (or test set)
+y_pred_dev = clf.predict(X_dev)
+
+# Calculate the accuracy
+accuracy = accuracy_score(y_dev, y_pred_dev)
+print(f"Baseline Accuracy on Dev set: {accuracy:.2f}")
+
+# If the dev set performance is satisfactory, you can evaluate on the test set
+y_pred_test = clf.predict(X_test)
+accuracy_test = accuracy_score(y_test, y_pred_test)
+print(f"Accuracy on Test set: {accuracy_test:.2f}")
+
+# For Random Forest:
+clf_rf = RandomForestClassifier(n_estimators=100, random_state=42)
+clf_rf.fit(X_train, y_train)
+y_pred_rf = clf_rf.predict(X_dev)
+
+# Confusion Matrix for Random Forest
+cm_rf = confusion_matrix(y_dev, y_pred_rf)
+plt.figure(figsize=(12, 10))
+sns.heatmap(cm_rf, annot=True, fmt='g', cmap='Blues')
+plt.title('Confusion Matrix for Random Forest')
+plt.xlabel('Predicted labels')
+plt.ylabel('True labels')
+plt.savefig("confusion_matrix_rf.png", dpi=300)
+plt.show()
+
+# Feature Importance Visualization
+plt.figure(figsize=(15, 5))
+plt.bar(range(len(X_train[0])), clf_rf.feature_importances_)
+plt.title('Feature Importances for Random Forest')
+plt.xlabel('Features')
+plt.ylabel('Importance Value')
+plt.savefig("feature_importances_rf.png", dpi=300)
+plt.show()
